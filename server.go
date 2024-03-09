3551264a0af6bdf34901bb99c61064582389a5e1
Thitisan K <thitisan.k@bluebik.com>
2024-03-09 23:15:38 +0700

    feat: build in go test.

---
+++ server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main()  {
+  
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    log.Println("Hello World!")   
+  })
+
+  http.ListenAndServe(":9000",nil)
+}
